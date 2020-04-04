75f751c04ee853bce3a33ae939e98c40895da3b3
dreamer <dreamer@dream.local>
2020-04-05 01:41:58 +0300

    angular features 1

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -8,48 +9,26 @@ import { Book } from 'src/app/common/book';
 })
 export class BookListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private bookService:BookService
 
-  books:Book[] = [
-    {
-      sku: "text-100",
-      name: "C Programming Language",
-      description: "Learn C Programming Language",
-      unitPrice: 600,
-      imageUrl: "assets/images/books/text-100.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-101",
-      name: "C# Crash Course",
-      description: "Learn C# Programming Language",
-      unitPrice: 900,
-      imageUrl: "assets/images/books/text-101.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-102",
-      name: "C++ Crash Course",
-      description: "Learn C++ Programming Language",
-      unitPrice: 700,
-      imageUrl: "assets/images/books/text-102.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    }
-  ]
+  ) { }
 
-
-
+  books:Book[] ;
 
   ngOnInit(): void {
+    this.listBooks();
   }
+
+
+
+  listBooks(){
+
+    this.bookService.getBooks().subscribe(
+      data => this.books = data
+    )
+  }
+
+
 
 }
--- onlinebookstore/src/main/java/com/yigitkader/onlinebookstore/repository/IBookRepository.java
+++ onlinebookstore/src/main/java/com/yigitkader/onlinebookstore/repository/IBookRepository.java
@@ -2,7 +2,9 @@ package com.yigitkader.onlinebookstore.repository;
 
 import com.yigitkader.onlinebookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:8081") //permisson for get request to angular | ('*' is give permission for any request)
 public interface IBookRepository extends JpaRepository<Book,Long> {
 
 
